13bfcc85aaa3849d01a423928cef89fe212263d2
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-07-27 01:16:49 +0700

    Initial Server

---
+++ controllers/UserController.js
@@ -0,0 +1,11 @@
+class UserController {
+    static register(req, res, next) {
+
+    }
+
+    static login(req, res, next) {
+
+    }
+}
+
+module.exports = UserController
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+    let statusCode;
+    let errors = []
+
+
+}
+
+module.exports = errorHandler
