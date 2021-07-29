2335c87159f1c5fecaa6627c3fd320f78bc89046
ardiansyakh <abdullah.ard9@gmail.com>
2021-07-29 10:59:49 +0700

    rev auth

--- router/admin/productRouter.js
+++ router/admin/productRouter.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 router.get('/', ProductController.readAll)
 router.post('/', ProductController.addProduct)
 router.get('/:id', ProductController.readDetail)
-router.put('/:id', ProductController.updateProduct)
+router.patch('/:id', ProductController.updateProduct)
 router.delete('/:id', ProductController.delete)
 
 module.exports = router
