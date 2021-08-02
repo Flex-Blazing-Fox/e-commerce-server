1b94aea504a5d125963aeb97f42a44c67fdc37e8
ardiansyakh <abdullah.ard9@gmail.com>
2021-08-02 21:36:05 +0700

    test

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,11 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const http = require('http')
 const app = require('../app')
 const PORT = process.env.PORT || 3000
 
+const server = http.createServer(app)
 
 app.listen(PORT, ()=>{
     console.log(`listen on port: ${PORT}`);
--- test/adminController.test.js
+++ test/adminController.test.js
@@ -1,12 +1,50 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('POST /admin/register', ()=>{
+beforeAll((done)=>{
+    queryInterface.bulkDelete('Users', null, {})
+    done()
+})
+
+afterAll((done)=>{
+    queryInterface.bulkDelete('Users', null, {})
+    done()
+})
+
+describe('POST /admin/login', ()=>{
     test('Success register', (done)=>{
         request(app)
             .post('/admin/register')
+            .send({
+                email: 'admin4@mail.com',
+                password: '1234'
+            })
             .then(response=>{
-                expect(response.statusCode).tobe(201)
+                expect(response.statusCode).toBe(201)
+                done()
+            })
+    }),
+    test('Email already registered', (done)=>{
+        request(app)
+            .post('/admin/register')
+            .send({
+                email: 'admin@mail.com',
+                password: '1234'
+            })
+            .then(response=>{
+                expect(response.statusCode).toBe(400)
+                done()
+            })
+    }),
+    test('Invalid email/password', (done)=>{
+        request(app)
+            .post('/admin/login')
+            .send({
+                email: 'admin@mail.com',
+                password: '123456'
+            })
+            .then(response=>{
+                expect(response.statusCode).toBe(401)
                 done()
             })
     })
