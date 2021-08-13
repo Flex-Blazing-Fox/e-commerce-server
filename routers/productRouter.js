bbc26c925e5b6bd1b9da2527111ff6da4f0603b5
juandari <juandaari70@gmail.com>
2021-08-13 17:59:03 +0700

    fix bugs

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -6,7 +6,7 @@ const ProductController = require('../controllers/ProductController')
 router.get('/', ProductController.getProducts)
 router.get('/:id', ProductController.getProduct)
 router.post('/', authenticate, authorize, ProductController.addProduct)
-router.put('/:id', authenticate, authorize, ProductController.editProduct)
+router.put('/:id', authenticate, ProductController.editProduct)
 router.delete('/:id', authenticate, authorize, ProductController.deleteProduct)
 
 module.exports = router
